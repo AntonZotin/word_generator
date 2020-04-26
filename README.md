deb78c92831d72684f43637104cbd40204d5a32c
Anton Zotin <toxin88@mail.ru>
2020-04-26 12:28:16 +0300

    add project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# word_generator
+# Установка сборщика
+
+1. pip install pypiwin32
+1. pip install pyinstaller
+1. pyinstaller --version
+
+# Сборка
+
+1. pyinstaller --onefile --windowed app.py
---
+++ app.py
@@ -0,0 +1,134 @@
+import sys
+
+import PySimpleGUI as Gui
+from docx import Document
+from docx.enum.style import WD_STYLE_TYPE
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.shared import Pt
+from datetime import datetime
+
+required_fields = {
+    'name': 'Наименование компании',
+    'inn': 'ИНН',
+    'number': 'Номер заявки',
+    'date': 'Дата заявки',
+    'has_comment': 'Соответствие заявки',
+    'comment': 'Комментарий',
+}
+
+numeric_fields = ['inn', 'number']
+
+tab = '            '
+text1 = 'Юридическим отделом государственного казенного учреждения «Центр реализации программ поддержки и ' \
+        'развития малого и среднего предпринимательства Республики Татарстан» (далее – Учреждение) проверена заявка '
+text2 = ' (ИНН '
+text3 = ') № '
+text4 = ' от '
+text5 = ' на предмет соответствия требованиям Порядка предоставления субсидий на возмещение ' \
+       'затрат, связанных с уплатой процентов по кредитам, привлеченным в российских кредитных организациях, ' \
+       'утвержденного постановлением Кабинета Министров Республики Татарстан от 24.09.2019 № 873 «Об утверждении ' \
+       'Порядка предоставления субсидий на возмещение затрат, связанных с уплатой процентов по кредитам, привлеченным ' \
+       'в российских кредитных организациях» (далее – Порядок).'
+success = 'По итогам проверки замечания не выявлены.'
+fail = 'По итогам проверки выявлены следующие замечания:'
+specialist = 'Главный специалист						              Е.Р. Зотина'
+footer = 'Дата: '
+
+HAS_NO_COMMENT = 'Соответствует'
+HAS_COMMENT = 'Не соответствует'
+
+
+def main_generate_word(name, inn, number, date, has_comment, comment):
+    if has_comment == HAS_COMMENT:
+        comment = comment.replace("\n", "\t\n" + tab)
+    paragraph1 = f'{tab}{text1}{name}{text2}{inn}{text3}{number}{text4}{date}{text5}\t\n' \
+        f'{tab}{fail if has_comment == HAS_COMMENT else success}\t\n' \
+        f'{tab}{comment if has_comment == HAS_COMMENT else ""}\t\n'
+    paragraph2 = f'{specialist}\n\n\n'
+    paragraph3 = f'{footer}{datetime.today().strftime("%d.%m.%Y")}'
+
+    document = Document()
+
+    obj_styles = document.styles
+    obj_charstyle = obj_styles.add_style('Main title', WD_STYLE_TYPE.CHARACTER)
+    obj_font = obj_charstyle.font
+    obj_font.size = Pt(14)
+    obj_font.name = 'Times New Roman'
+    obj_charstyle = obj_styles.add_style('Middle paragraph', WD_STYLE_TYPE.CHARACTER)
+    obj_font = obj_charstyle.font
+    obj_font.size = Pt(12)
+    obj_font.name = 'Times New Roman'
+    obj_charstyle = obj_styles.add_style('Last paragraph', WD_STYLE_TYPE.CHARACTER)
+    obj_font = obj_charstyle.font
+    obj_font.size = Pt(8)
+    obj_font.name = 'Times New Roman'
+
+    t = document.add_paragraph('')
+    t.add_run('ЗАКЛЮЧЕНИИЕ ЮРИДИЧЕСКОГО ОТДЕЛА', style='Main title').bold = True
+    t.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    p1 = document.add_paragraph('')
+    p1.add_run(paragraph1, style='Middle paragraph')
+    p1.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+
+    p2 = document.add_paragraph('')
+    p2.add_run(paragraph2, style='Middle paragraph').bold = True
+    p2.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    p3 = document.add_paragraph('')
+    p3.add_run(paragraph3, style='Last paragraph')
+    p3.alignment = WD_PARAGRAPH_ALIGNMENT.LEFT
+
+    document.save("%s %s.docx" % (name.replace('\"', '\''), number[-4:]))
+
+
+def main():
+    layout = [
+        [Gui.Text('Наименование компании', size=(20, 1)), Gui.InputText(size=(42, 1), key='name')],
+        [Gui.Text('ИНН', size=(20, 1)), Gui.Input(size=(42, 1), key='inn', enable_events=True)],
+        [Gui.Text('Номер заявки', size=(20, 1)), Gui.Input(size=(42, 1), key='number', enable_events=True)],
+        [Gui.Text('Дата заявки', size=(20, 1)), Gui.Input(size=(42, 1), key='date', enable_events=True)],
+        [Gui.Text('_' * 68)],
+        [Gui.Text('Соответствие заявки', size=(20, 1)), Gui.Combo([HAS_NO_COMMENT, HAS_COMMENT], size=(40, 1),
+                                                                  readonly=True, key='has_comment')],
+        [Gui.Text('Комментарий', size=(20, 1)), Gui.Multiline(size=(40, 10), key='comment')],
+        [Gui.Text('', size=(25, 1)), Gui.Submit(button_text='Сгенерировать')]
+    ]
+
+    window = Gui.Window('Word generator', layout)
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event in (None, 'Exit', 'Cancel'):
+            return 0
+        elif event in numeric_fields and values[event] and values[event][-1] not in '0123456789':
+            window[event].update(values[event][:-1])
+        elif event == 'date' and values[event]:
+            if len(values[event]) == 2 or len(values[event]) == 5:
+                window[event].update(values[event] + '.')
+            elif len(values[event]) == 11:
+                window[event].update(values[event][:-1])
+        elif event == 'Сгенерировать':
+            required_errors = []
+            name = values['name']
+            if not name: required_errors.append(required_fields['name'])
+            inn = values['inn']
+            if not inn: required_errors.append(required_fields['inn'])
+            number = values['number']
+            if not number: required_errors.append(required_fields['number'])
+            date = values['date']
+            if not date: required_errors.append(required_fields['date'])
+            has_comment = values['has_comment']
+            if not has_comment: required_errors.append(required_fields['has_comment'])
+            comment = values['comment'].strip()
+            if not comment and has_comment: required_errors.append(required_fields['comment'])
+
+            if not required_errors:
+                main_generate_word(name, inn, number, date, has_comment, comment)
+            else:
+                Gui.popup('Вы не ввели обязательные поля:\n%s' % ', '.join(required_errors), title='Пустые поля')
+
+
+if __name__ == '__main__':
+    sys.exit(main())
